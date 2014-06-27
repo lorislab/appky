fb66bf1f375d59a1967ea02f0952c4b17685d22a
Andrej Petras <andrej@ajka-andrej.com>
2014-06-27 18:47:01 +0200

    Update

--- appky-process/src/main/java/org/lorislab/appky/process/config/ViewConfiguration.java
+++ appky-process/src/main/java/org/lorislab/appky/process/config/ViewConfiguration.java
@@ -15,20 +15,12 @@
  */
 package org.lorislab.appky.process.config;
 
-import java.io.Serializable;
-
-
 /**
  * The public login configuration.
  *
  * @author Andrej Petras <andrej@ajka-andrej.com>
  */
-public class ViewConfiguration implements Serializable {
-
-    /**
-     * The UID for this class.
-     */
-    private static final long serialVersionUID = 72168413623648302L;
+public class ViewConfiguration {
 
     /**
      * The registration public key.
